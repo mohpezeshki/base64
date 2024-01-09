d69f2f0c6d26b367f39fdef185dfb2441abe3f40
mohammad <mohammad.pezeshki1994@gmail.com>
2024-01-09 16:42:34 +0100

    debug encode function

--- base64.py
+++ base64.py
@@ -5,14 +5,14 @@ BASE64_TABLE = string.ascii_uppercase + string.ascii_lowercase + string.digits  
 
 PADDING = "="
 
-def code_base64(text):
+def encode_base64(text):
     b_str = ''.join(format(ord(i), '08b') for i in text)
     base64_arr = []
     for i in range(len(b_str)//6):
         base64_arr.append(b_str[i*6:i*6+6])
     
     if len(b_str) % 6 != 0 :
-        base64_arr.append(b_str[-len(b_str)%6:])
+        base64_arr.append(b_str[-(len(b_str)%6):])
 
     base64_arr = [int(item , base=2) for item in base64_arr]
     base64_str =  ''.join([BASE64_TABLE[item] for item in base64_arr])
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         print("Insert the text!")
     else:
         if sys.argv[1] == "encode":
-            base64_txt = code_base64(sys.argv[2])
+            base64_txt = encode_base64(sys.argv[2])
         elif(sys.argv[1]) == "decode":
             base64_txt = decode_base64(sys.argv[2])
         
