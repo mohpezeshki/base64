abbbaabd3e8a52867373875954982b982f916047
mohammad <mohammad.pezeshki1994@gmail.com>
2024-01-10 16:44:38 +0100

    encode file

--- base64.py
+++ base64.py
@@ -1,18 +1,27 @@
 import string
 import sys
+import os
 
-BASE64_TABLE = string.ascii_uppercase + string.ascii_lowercase + string.digits  + '/'
+
+BASE64_TABLE = string.ascii_uppercase + string.ascii_lowercase + string.digits  + '+' + '/'
 
 PADDING = "="
 
-def encode_base64(text):
-    b_str = ''.join(format(ord(i), '08b') for i in text)
+def get_string_bits(text):
+    return  ''.join(format(ord(i), '08b') for i in text)
+ 
+
+def encode_base64(b_str):
     base64_arr = []
     for i in range(len(b_str)//6):
         base64_arr.append(b_str[i*6:i*6+6])
     
     if len(b_str) % 6 != 0 :
-        base64_arr.append(b_str[-(len(b_str)%6):])
+        if len(b_str) % 6 == 4:
+            _temp = b_str[-(len(b_str)%6):] + '00'
+        else:
+            _temp = b_str[-(len(b_str)%6):] + '0000'
+        base64_arr.append(_temp)
 
     base64_arr = [int(item , base=2) for item in base64_arr]
     base64_str =  ''.join([BASE64_TABLE[item] for item in base64_arr])
@@ -25,7 +34,6 @@ def encode_base64(text):
     return base64_str
 
 def decode_base64(text):
-   
    
     bit_array = [format(BASE64_TABLE.find(i) , '08b') for i in text.replace(PADDING , "")]     
     
@@ -42,15 +50,27 @@ def decode_base64(text):
     bit_str = ''.join(bit_array)
     return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(bit_str)]*8))
 
+def get_file_bits(file_path):
+    with open(file_path , 'rb') as f:
+        contents = f.read()
+    return ''.join([format(x, '08b') for x in contents])
+
 
 if __name__ == "__main__":
     
     base64_txt = "INSERT encode OR decode COMMAND!"
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Insert the text!")
     else:
+        _typ = "string"
+        if len(sys.argv) == 4 and sys.argv[2] == "file":
+            _typ = "file"
+            bit_string = get_file_bits(sys.argv[3])
         if sys.argv[1] == "encode":
-            base64_txt = encode_base64(sys.argv[2])
+            if _typ == "string": 
+                base64_txt = encode_base64(get_string_bits(sys.argv[2]))
+            else:
+                base64_txt = encode_base64(bit_string)
         elif(sys.argv[1]) == "decode":
             base64_txt = decode_base64(sys.argv[2])
         
